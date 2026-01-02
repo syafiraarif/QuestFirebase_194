e20947e4cdba30e0c0df4fdae3d974ad3920b5f6
syairaarif <putrisyafira4444@gmail.com>
2026-01-02 13:30:59 +0700

    menyimpan state di ui yang berkaitan dengan data siswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -20,7 +20,16 @@ fun DetailSiswa.toDataSiswa(): Siswa =
 fun Siswa.toDetailSiswa(): DetailSiswa =
     DetailSiswa(id, nama, alamat, telpon)
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
 
-
+fun Siswa.toUiStateSiswa(
+    isEntryValid: Boolean = false
+): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
 
 
