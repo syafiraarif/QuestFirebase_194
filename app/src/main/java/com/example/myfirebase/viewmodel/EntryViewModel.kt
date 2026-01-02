48150e70c9dbcb9ada292dc0837cb7a5d3cc4ce1
syairaarif <putrisyafira4444@gmail.com>
2026-01-02 13:56:52 +0700

    entry view model digunakan untuk data ketika di input dimana ada 2 variabel yang digunakan yaitu var uistatesiswa sama repositori siswa

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+
+class EntryViewModel(
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+}
