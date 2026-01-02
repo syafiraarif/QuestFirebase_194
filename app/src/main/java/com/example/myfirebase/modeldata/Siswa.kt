1c592e3b2c39064a939979ff763804c387cf2dfb
syairaarif <putrisyafira4444@gmail.com>
2026-01-02 13:27:38 +0700

    membuat model data untuk UI, dimana model ini digunain biasanya di form input atau edit

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -7,3 +7,12 @@ data class Siswa(
     val telpon: String = ""
 )
 
+data class DetailSiswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+
+
