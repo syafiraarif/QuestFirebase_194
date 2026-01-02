609ae341d1b502ea9161e1d406da44f2fbd3c429
syairaarif <putrisyafira4444@gmail.com>
2026-01-02 13:43:48 +0700

    Menginisialisasi FirebaseRepositorySiswa menggunakan lazy, dimana  lazy digunakan untuk memastikan repository dibuat hanya saat dibutuhkan, lebih efisien.

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -3,3 +3,9 @@ package com.example.myfirebase.repositori
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
