8729c8646670b794ddf30143e11b77a1e70c70f6
syairaarif <putrisyafira4444@gmail.com>
2026-01-02 13:44:56 +0700

    membuat container app untuk repositorysiswa

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -1,5 +1,7 @@
 package com.example.myfirebase.repositori
 
+import android.app.Application
+
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
@@ -7,5 +9,14 @@ interface ContainerApp {
 class DefaultContainerApp : ContainerApp {
     override val repositorySiswa: RepositorySiswa by lazy {
         FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
     }
 }
