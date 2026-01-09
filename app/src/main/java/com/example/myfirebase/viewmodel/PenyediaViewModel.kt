05484e3e902794d2d04626a16d555cc85c8829ec
syairaarif <putrisyafira4444@gmail.com>
2026-01-09 10:16:01 +0700

    memanggil viewmodeldetail yang sudah di buat agar dia bisa di akses lewat satu pintu yaitu penyediaviewmodel

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.myfirebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -20,6 +21,11 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(
                 aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa
             )
         }
     }
