a8ef79457f39c3986c29c1b68aef2bdedd4f906a
syairaarif <putrisyafira4444@gmail.com>
2026-01-09 09:04:06 +0700

    menambahkan variabel idsiswa untukmengambil data siswa dari firebase

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -18,5 +18,16 @@ class EditViewModel(
     var uiStateSiswa by mutableStateOf(value = UIStateSiswa())
         private set
 
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
 
 }
