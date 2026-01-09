5c45a6add015cccd7274239e04663ea900ad0d0d
syairaarif <putrisyafira4444@gmail.com>
2026-01-09 09:49:19 +0700

    variabel untuk menyimpan kondisi layarnya

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -30,7 +30,12 @@ class DetailViewModel(
         savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
             ?: error("idSiswa tidak ditemukan di SavedStateHandle")
 
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
 
+    init {
+        getSatuSiswa()
+    }
 
 }
 
