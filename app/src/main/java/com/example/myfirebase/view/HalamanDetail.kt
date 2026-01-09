e97198c312637b9f475c4ce59e8b06f56fc2d6db
syairaarif <putrisyafira4444@gmail.com>
2026-01-09 10:43:16 +0700

    menambahkan fungsi baris detail yang memiliki 3 parameter

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -1,7 +1,10 @@
 package com.example.myfirebase.view
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -26,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myfirebase.view.route.DestinasiDetail
 import com.example.myfirebase.viewmodel.DetailViewModel
@@ -167,3 +171,23 @@ fun DetailDataSiswa(
         }
     }
 }
+
+@Composable
+private fun BarisDetailData(
+    @StringRes labelResID: Int,
+    itemDetail: String,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(
+            text = stringResource(labelResID),
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = itemDetail,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
