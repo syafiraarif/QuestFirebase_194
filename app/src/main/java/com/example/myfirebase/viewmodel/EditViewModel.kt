6bdd502705af48d0f30740dc168e03362d57fe98
syairaarif <putrisyafira4444@gmail.com>
2026-01-09 09:02:02 +0700

    membuat class edit view model yang memiliki 2 parameter, yaitu untuk memanggil repositorysiswa dan juga savehandale untuk ambil id dari navigasi

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+}
