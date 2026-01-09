4b3f19244cacf5c63bb3a26ddf0cf4035b964adc
syairaarif <putrisyafira4444@gmail.com>
2026-01-09 08:57:26 +0700

    memanggil fungsi hapusatusiswa dan membuat implementasinya

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -80,4 +80,11 @@ class FirebaseRepositorySiswa : RepositorySiswa {
             )
         ).await()
     }
+
+    override suspend fun hapusSatuSiswa(id: Long) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+
+        collection.document(docId).delete().await()
+    }
 }
